8bbe187e82424c28a399a3b0c929b4f507b92e02
Sohini Das <dsohini200@gmail.com>
2024-07-26 13:24:48 +0530

    Loader added

--- nail-blog/src/components/Loader.jsx
+++ nail-blog/src/components/Loader.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { InfinitySpin } from "react-loader-spinner";
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="h-screen w-screen text-center">
+      <InfinitySpin height="100%" width="100%" color="#183155" />
+    </div>
+  );
 };
 
 export default Loader;

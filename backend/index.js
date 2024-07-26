7a70d33139083582627130345b6c9920f1f7ef30
Sohini Das <dsohini200@gmail.com>
2024-07-27 02:25:24 +0530

    user model and signup route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -13,5 +16,8 @@ mongoose
 
 const app = express();
 app.listen(8000, () => {
-  console.log("Server is running on port 5000!!");
+  console.log("Server is running on port 8000!!");
 });
+app.use(express.json());
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);

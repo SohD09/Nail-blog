8b4712cdc514fb3034106fe40964bcd2edec2940
Sohini Das <dsohini200@gmail.com>
2024-07-26 13:14:21 +0530

    pages and routes

--- nail-blog/src/App.jsx
+++ nail-blog/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Signin = lazy(() => import("./pages/Signin"));
+const Signup = lazy(() => import("./pages/Signup"));
 
 const App = () => {
-  return <h1 className="text-blue-950">App. Hi</h1>;
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<Projects />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;

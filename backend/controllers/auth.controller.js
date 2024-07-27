36ce223001555a392d285a749f3eaae3c96671f5
Sohini Das <dsohini200@gmail.com>
2024-07-28 01:19:35 +0530

    signup api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   if (
@@ -11,10 +12,11 @@ export const signup = async (req, res) => {
   ) {
     return res.status(400).json({ message: "All fields are required" });
   }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
 
   try {

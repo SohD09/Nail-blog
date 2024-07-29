820db1414947e8df6f2f46c198501f8767c7549e
Sohini Das <dsohini200@gmail.com>
2024-07-29 14:57:41 +0530

    functionality of sign-up page

--- nail-blog/src/pages/Signup.jsx
+++ nail-blog/src/pages/Signup.jsx
@@ -1,9 +1,43 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import signupbg from "../assets/images/signupbg.png";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      setSuccessMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) return setErrorMessage(data.message);
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
   return (
     <div className=" min-h-[calc(100vh-100px)] relative flex justify-center items-center bg-gradient-to-tr from-pastel-magenta to-classic-rose xl:bg-none">
       <img
@@ -11,7 +45,7 @@ const Signup = () => {
         alt="bg"
         className="fixed top-0 left-0 object-fill -z-10 opacity-0 xl:opacity-100 "
       />
-      <div className=" bg-white absolute h-[50%] w-[60%] flex rounded-lg shadow-gray-400 p-5 shadow-lg bg-opacity-45 xl:bg-opacity-40 xl:h-[70%]">
+      <div className=" bg-white absolute min-h-[50%] w-[60%] flex rounded-lg shadow-gray-400 p-5 shadow-lg bg-opacity-45 xl:bg-opacity-40 xl:min-h-[70%]">
         <div className=" flex p-3 max-w-3xl mx-auto my-auto flex-col lg:flex-row lg:items-center gap-5 ">
           <div className="flex-1">
             <Link
@@ -29,10 +63,15 @@ const Signup = () => {
             </p>
           </div>
           <div className="flex-1">
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
               <div>
                 <Label value="Your username" />
-                <TextInput type="text" placeholder="Username" id="username" />
+                <TextInput
+                  type="text"
+                  placeholder="Username"
+                  id="username"
+                  onChange={handleChange}
+                />
               </div>
               <div>
                 <Label value="Your email" />
@@ -40,6 +79,7 @@ const Signup = () => {
                   type="email"
                   placeholder="name@company.com"
                   id="email"
+                  onChange={handleChange}
                 />
               </div>
               <div>
@@ -48,10 +88,22 @@ const Signup = () => {
                   type="password"
                   placeholder="Password"
                   id="password"
+                  onChange={handleChange}
                 />
               </div>
-              <Button gradientDuoTone="purpleToBlue" type="submit">
-                Signup
+              <Button
+                gradientDuoTone="purpleToBlue"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <Spinner size="sm"></Spinner>
+                    <span>Loading...</span>
+                  </>
+                ) : (
+                  "Signup"
+                )}
               </Button>
             </form>
             <div className="flex gap-2 mt-5 text-sm sm:text-base ">
@@ -60,6 +112,12 @@ const Signup = () => {
                 Signin
               </Link>
             </div>
+            {errorMessage && (
+              <Alert className="mt-2 bg-red-300 text-red-950" color="red">
+                {errorMessage}
+                {console.log(errorMessage)}
+              </Alert>
+            )}
           </div>
         </div>
       </div>
